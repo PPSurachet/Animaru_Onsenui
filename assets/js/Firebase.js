5439c0d8f2ac212264167bd7edeac4bed237dc8f
Doublepor <doublepor.pp@gmail.com>
2020-11-03 21:50:51 +0700

    Set Project

---
+++ assets/js/Firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCez0bnJ60odWOo8kbXGYJpQFcqbxDt3pI",
+    authDomain: "animaru-4aea5.firebaseapp.com",
+    databaseURL: "https://animaru-4aea5.firebaseio.com",
+    projectId: "animaru-4aea5",
+    storageBucket: "animaru-4aea5.appspot.com",
+    messagingSenderId: "1066577428240",
+    appId: "1:1066577428240:web:a636233b70aa19e06eaf95",
+    measurementId: "G-JSGHXTEEW0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
