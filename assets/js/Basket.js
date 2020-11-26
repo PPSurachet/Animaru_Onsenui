6118c37672aa6472b152f336cb933a35ac6ea737
Doublepor <doublepor.pp@gmail.com>
2020-11-26 14:08:07 +0700

    Set Js all page

---
+++ assets/js/Basket.js
@@ -0,0 +1,9 @@
+document.addEventListener('init', function (event) {
+    var page = event.target;
+
+    if (page.id === 'BasketPage') {
+
+    } else if (page.id === 'paymentPage') {
+
+    }
+});
---
+++ assets/js/Home.js
@@ -0,0 +1,11 @@
+document.addEventListener('init', function (event) {
+    var page = event.target;
+
+    if (page.id === 'HomePage') {
+
+    } else if (page.id === 'ShopPage') {
+
+    } else if (page.id === 'SelectPage') {
+
+    }
+});
