e25f7466b4fce3346a2ace98fbc45d26877f71fe
wattana <endnapadad25@gmail.com>
2020-11-28 03:16:52 +0700

    add_basket2

--- assets/js/Basket.js
+++ assets/js/Basket.js
@@ -2,7 +2,7 @@ document.addEventListener('init', function (event) {
     var page = event.target;
 
     if (page.id === 'BasketPage') {
-
+       
     } else if (page.id === 'paymentPage') {
 
     }
