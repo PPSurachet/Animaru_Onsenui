f5bee482080f23505b7deb490fef256cd51ef566
[key] <[key.cmdark@gmail.com]>
2020-11-29 20:46:44 +0700

    first commit add setting

--- assets/js/Setting.js
+++ assets/js/Setting.js
@@ -2,10 +2,18 @@ document.addEventListener('init', function (event) {
     var page = event.target;
 
     if (page.id === 'SettingPage') {
-
+        
     } else if (page.id === 'myShopPage') {
 
     } else if (page.id === 'profilePage') {
 
     }
 });
+
+function profilePage() {
+    document.querySelector('#Navigator_setting').pushPage("views/Setting/Profile.html");
+}
+
+function backtoProfilePage() {
+    document.querySelector('#Navigator_setting').popPage();
+}
