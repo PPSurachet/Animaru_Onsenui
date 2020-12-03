475f92fb6fd5fb9f36f3ed7ef7b289c86943dc5f
Doublepor <doublepor.pp@gmail.com>
2020-12-03 16:48:28 +0700

    Sign in google

--- assets/js/Login.js
+++ assets/js/Login.js
@@ -1,6 +1,37 @@
 $(function () {
     LoginByEmail();
+    loginByGoogle();
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            window.location.href = "Animaru.html"
+        }
+    });
 })
+
+const loginByGoogle = () => {
+    $("#signInGoogle").click(function () {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithRedirect(provider);
+        firebase.auth().getRedirectResult().then(function (result) {
+            if (result.credential) {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+                // ...
+            }
+            // The signed-in user info.
+            var user = result.user;
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+    })
+}
 
 const LoginByEmail = () => {
     $("#signIn").click(function () {
